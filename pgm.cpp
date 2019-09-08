8a656f49d32ee72779044eae4dd04073b903666d
MibettyMeanie <crazymeens@gmail.com>
2019-09-08 07:21:08 +0530

    File handling 2

--- pgm.cpp
+++ pgm.cpp
@@ -11,8 +11,8 @@ class Details{
 	string berth_pref;
 public:
 	void get_details(){
-		fstream file_obj;
-		file_obj.open("Railway.rtf",ios::binary|ios::out|ios::app);
+
+		
 		cout<<"Enter name - ";
 		getline(cin,name);
 		cout<<"Enter Age - ";
@@ -23,63 +23,77 @@ public:
 		cin.get();
 		getline(cin,berth_pref);
 		cout<<endl;
-		file_obj.write((char*)this, sizeof(*this)); 
 	}
 
 	void show_details(){
-		fstream file_obj;
-		Details obj;
-		file_obj.open("Railway.rtf",ios::binary|ios::in);
-		file_obj.read((char*)&obj, sizeof(obj)); 
-
-		cout<<"Name: "<<obj.name<<" "<<endl;
-		cout<<"Age: "<<obj.age<<" "<<endl;
-		cout<<"Gender: "<<obj.gender<<" "<<endl;
-		cout<<"Berth preference: "<<obj.berth_pref<<endl;
-		cout<<endl;
-
-	}
-
-};
-
-class Railway{
-	int count;
-	Details Passen[100];	
-public:
-	Railway(){
-		count=-1;
-	}
-	void book(int n){
 		
-		for(int i=count+1;i<n+count+1;i++){
-			cout<<"Enter details of Passenger "<<i+1<<endl;
-			Passen[i].get_details();
-		}
-		count+=n;
+		cout<<"Name: "<<name<<" "<<endl;
+		cout<<"Age: "<<age<<" "<<endl;
+		cout<<"Gender: "<<gender<<" "<<endl;
+		cout<<"Berth preference: "<<berth_pref<<endl;
+		cout<<endl;	
+
 	}
 
-	void print(){
-
-		for(int i=0;i<count+1;i++){
-			cout<<"Details of Passenger "<<i+1<<endl;
-			cout<<endl;
-			Passen[i].show_details();	
-		}
-	}
 };
-
-
 int main(){
 
-	int n;
-	Railway R;
-	cout<<"Enter the no: of tickets - ";
-	cin>>n;
-	cin.get();
-	cout<<endl;
-	R.book(n);
-	R.print();
-	return 0;
+	int n,x;
+	Details D[100];
+	fstream file_obj,to_write;
+	file_obj.open("Railway_sys.rtf",ios::binary|ios::in);
+	int counter=-1;
+	while(!file_obj.eof()){
+
+		file_obj.read((char*)&D[counter], sizeof(D[counter])); 
+		counter++;
+
+	}
 
 
+	do{
+
+		cout<<"Enter 1 to book ticket"<<endl;
+		cout<<"Enter 2 to show all booked tickets"<<endl;
+		cout<<"Enter 3 to exit"<<endl;
+		cout<<"Enter your option: ";
+		cin>>n;
+		switch(n){
+
+			case 1: 
+				cout<<"Enter the number of tickets you want to book - ";
+				cin>>x;
+				cin.get();
+				for(int i=counter;i<x+counter;i++){
+					cout<<"Enter details of Passenger "<<endl;
+					D[i].get_details();
+				}
+				counter+=x;
+				break;
+
+			case 2:
+				for(int i=0;i<counter;i++){
+					cout<<"Details of Passenger "<<i+1<<endl;
+					D[i].show_details();
+				}
+				break;
+
+			case 3:
+
+				to_write.open("Railway_sys.rtf",ios::binary|ios::out|ios::app);
+				for(int i=0;i<counter;i++){
+					Details P= D[counter];
+					to_write.write((char*)&P, sizeof(P));
+
+				}
+				char y= 'p';
+				to_write>>y;
+				to_write.close();
+				file_obj.close();
+				return 0;
+
+		}
+
+	}while(1);
+	
 }
